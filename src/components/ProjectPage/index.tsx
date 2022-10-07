a5d3e2046e787493c643c4c9b8d470cbc9ea5adc
Rohan Gupta <rohan.gupta@gameskraft.com>
2022-10-07 10:33:06 +0530

    Create ProjectPage component

---
+++ src/components/ProjectPage/index.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import classes from './styles.module.scss';
+
+const ProjectPage: React.FC<ProjectPageProps> = (props) => {
+  const {
+    background = '',
+    pageClassName = '',
+    pageStyle = {},
+    containerClassName = '',
+    containerStyle = {},
+    children,
+  } = props;
+
+  return (
+    <main className={`${classes.page} ${pageClassName}`} style={{ ...pageStyle, background }}>
+      <section className={`${classes.contentContainer} ${containerClassName}`} style={containerStyle}>
+        {children}
+      </section>
+    </main>
+  );
+};
+
+export default ProjectPage;
