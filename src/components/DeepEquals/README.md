9ef73c7ad7efd635b9938e805366390184a1e716
Rohan Gupta <39908472+DemonDaddy22@users.noreply.github.com>
2022-11-09 22:29:22 +0530

    🚀 Implement Deep Equals exercise (#63)
    Co-authored-by: Rohan Gupta <rohan.gupta@gameskraft.com>

---
+++ src/components/DeepEquals/README.md
@@ -0,0 +1,33 @@
+# Deep Equals
+
+_Tag_: **JavaScript**
+
+Write a deepEquals function that takes in two required values and returns if those two values are deeply equal to each other.
+
+For the purpose of this problem, deep equality is defined as follows:
+
+- Values with different types are not equal.
+- NaN is only equal to NaN.
+- null is only equal to null, and undefined is only equal to undefined. These values are not equal to each other.
+- Arrays are only equal if their entries are deeply equal to each other.
+- Objects are equal only if their keys and values are deeply equal to each other (note that the order of the keys doesn't matter).
+
+You can make the following assumptions:
+
+- Functions will never be passed to deepEquals and will never be contained in objects or arrays passed to deepEquals.
+- Objects will only have string keys, and their values won't be recursive references to themselves.
+- The prototype chain doesn't need to be considered when determining if two objects are deeply equal.
+
+```javascript
+deepEquals(1, 1); // true
+deepEquals(1, '1'); // false
+deepEquals(null, null); // true
+deepEquals(null, undefined); // false
+deepEquals([], []); // true
+deepEquals({}, {}); // true
+deepEquals([], {}); // false
+deepEquals({ a: 123, b: { c: [4, 5, 6] } }, { a: 123, b: { c: [4, 5, 6] } }); // true
+deepEquals({ a: 123, b: { c: [4, 5, 6] } }, { b: { c: [4, 5, 6] } }); // false
+deepEquals([1, 2, [3, 4]], [1, 2, [3, 4]]); // true
+deepEquals([1, 2, [3, 4, { a: 'abc' }]], [1, 2, [3, 4, { a: 'abc' }]]); // true
+```
---
+++ src/components/DeepEquals/index.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { PROJECT_COLORS } from '../../constants/theme';
+import ProjectPage from '../ProjectPage';
+import QuestionDetails from '../QuestionDetails';
+import CodeBlock from '../UI/CodeBlock';
+import { sampleUsage, solutionCode } from './codeBlocks';
+import classes from './styles.module.scss';
+
+const DeepEquals: React.FC<Props> = () => {
+  return (
+    <ProjectPage background={PROJECT_COLORS.PROJECT22.background} containerClassName={classes.pageContainer}>
+      <QuestionDetails
+        title='Deep Equals'
+        titleClassName={classes.questionTitle}
+        containerClassName={classes.questionDetails}
+        descriptionClassName={classes.questionDescription}
+      >
+        <p>
+          Write a <span className={classes.questionDescriptionHighlight}>deepEquals</span> function that takes in two
+          required values and returns if those two values are deeply equal to each other.
+        </p>
+        <p>For the purpose of this problem, deep equality is defined as follows:</p>
+        <ul>
+          <li>Values with different types are not equal.</li>
+          <li>
+            <span className={classes.questionDescriptionHighlight}>NaN</span> is only equal to{' '}
+            <span className={classes.questionDescriptionHighlight}>NaN</span>.
+          </li>
+          <li>
+            <span className={classes.questionDescriptionHighlight}>null</span> is only equal to{' '}
+            <span className={classes.questionDescriptionHighlight}>null</span>, and{' '}
+            <span className={classes.questionDescriptionHighlight}>undefined</span> is only equal to{' '}
+            <span className={classes.questionDescriptionHighlight}>undefined</span>. These values are not equal to each
+            other.
+          </li>
+          <li>Arrays are only equal if their entries are deeply equal to each other.</li>
+          <li>
+            Objects are equal only if their keys and values are deeply equal to each other (note that the order of the
+            keys doesn't matter).
+          </li>
+        </ul>
+        <p>You can make the following assumptions:</p>
+        <ul>
+          <li>
+            Functions will never be passed to <span className={classes.questionDescriptionHighlight}>deepEquals</span>{' '}
+            and will never be contained in objects or arrays passed to{' '}
+            <span className={classes.questionDescriptionHighlight}>deepEquals</span>.
+          </li>
+          <li>Objects will only have string keys, and their values won't be recursive references to themselves.</li>
+          <li>The prototype chain doesn't need to be considered when determining if two objects are deeply equal.</li>
+        </ul>
+        <div className={classes.codeWrapper}>
+          <h3 className={classes.codeBlockHeader}>Sample Usage</h3>
+          <CodeBlock codeString={sampleUsage} />
+        </div>
+      </QuestionDetails>
+      <section className={classes.solutionContainer}>
+        <CodeBlock codeString={solutionCode} containerClassName={classes.fullHeight} />
+      </section>
+    </ProjectPage>
+  );
+};
+
+export default DeepEquals;
