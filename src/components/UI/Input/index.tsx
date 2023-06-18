32d0f54be412651898ea1629238f0b819d63c954
Rohan Gupta <rohan.gupta@gameskraft.com>
2023-06-18 15:41:04 +0530

    Update default styles of input component

--- src/components/UI/Input/index.tsx
+++ src/components/UI/Input/index.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import classes from './styles.module.scss';
 
-const Input: React.FC<InputProps> = (props) => {
+const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
   const { fullWidth = true, className, ...restProps } = props;
 
   return (
     <input
+      ref={ref}
       className={`${classes.input} ${className} ${
         fullWidth ? classes.fullWidth : ''
       }`}
       {...restProps}
     />
   );
-};
+});
 
 export default Input;
