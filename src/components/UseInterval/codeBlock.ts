fc29c8824213a55c8942c07663173aceedb98099
Rohan Gupta <rohan.gupta@gameskraft.com>
2023-11-29 09:52:05 +0530

    Implement useInterval exercise

--- src/components/UseInterval/codeBlock.ts
+++ src/components/UseInterval/codeBlock.ts
@@ -19,4 +19,27 @@ setFunc(() => {
 // After 2 more seconds, Second is logged
 `;
 
-export const solutionCode = `// useInterval hook`;
+export const solutionCode = `// useInterval hook
+
+import { useEffect, useRef } from 'react';
+
+const useInterval = (callback: () => void, delay?: number) => {
+  const callbackRef = useRef(callback);
+
+  useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    let intervalId: NodeJS.Timer | undefined;
+    if (delay) {
+      clearInterval(intervalId);
+      intervalId = setInterval(callbackRef.current, delay);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [delay]);
+};
+
+export default useInterval;
+`;
